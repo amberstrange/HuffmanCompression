3b48a22bc713d290bb153465dcb827c9fef673f5
User <User@DESKTOP-BVSC5JR>
2016-12-11 16:28:39 -0500

    acs79

--- src/HuffProcessor.java
+++ src/HuffProcessor.java
@@ -1,3 +1,4 @@
+import java.util.PriorityQueue;
 
 /**
  *	Interface that all compression suites must implement. That is they must be
@@ -29,15 +30,97 @@ public class HuffProcessor {
 	 * @param out
 	 *            Buffered bit stream writing to the output file.
 	 */
+	
 	public void compress(BitInputStream in, BitOutputStream out){
-	    while (true){
-	        int val = in.readBits(BITS_PER_WORD);
-	        if (val == -1) break;
-	        
-	        out.writeBits(BITS_PER_WORD, val);
-	    }
-	}
+	    int[] counts = readForCounts(in);
+	    TreeNode root = makeTreeFromCounts(counts);
+	    String[] codings = makeCodingsFromTree(root);
+	 
+	    writeHeader(root,out);
+	 
+	    in.reset();
 
+
+	    writeCompressedBits(in,codings,out);
+	}
+	
+	public int[] readForCounts(BitInputStream in){
+		int[] answer =new int[256];
+		while(true){
+			int bit = in.readBits(BITS_PER_WORD);
+			if(bit==-1) break;
+			answer[bit]++;
+		}
+		return answer;
+	}
+	public TreeNode makeTreeFromCounts(int[] freq){
+		PriorityQueue<TreeNode> pq = new PriorityQueue<TreeNode>();
+		for(int i =0; i<256; i++){
+			if(freq[i]!=0){
+				pq.add(new TreeNode(i, freq[i]));
+			}
+		}
+		pq.add(new TreeNode(PSEUDO_EOF,1));
+		while(pq.size()>1){
+			TreeNode left = pq.remove();
+			TreeNode right = pq.remove();
+			TreeNode t = new TreeNode(-1, left.myWeight + right.myWeight, left, right);
+			pq.add(t);
+		}
+		TreeNode root = pq.remove();
+		return root;
+	}
+	public boolean isLeafNode(TreeNode t){
+		return (t.myValue>=0&&t!=null);
+	}
+	public String[] makeCodingsFromTree(TreeNode t){
+		String[] answer = new String[257];
+		makeCodingsHelper(t, "", answer);
+		return answer;
+		
+	}
+	public void makeCodingsHelper(TreeNode root, String traversal, String[]codings ){
+		if(isLeafNode(root)){
+			codings[root.myValue]=traversal;
+			return;
+		}
+		if(root.myLeft!=null){
+			makeCodingsHelper(root.myLeft, traversal + "0", codings);
+		}
+		if(root.myRight!=null){
+			makeCodingsHelper(root.myRight, traversal + "1", codings);
+		}
+		
+	}
+	public void writeHeader(TreeNode t,BitOutputStream out){
+		out.writeBits(32, HUFF_TREE);
+		writeTree(t, out);
+	}
+	public void writeTree(TreeNode t, BitOutputStream out){
+		if(isLeafNode(t)){
+			out.writeBits(1,1);
+			out.writeBits(9, t.myValue);
+			return;
+		}
+		out.writeBits(1, 0);
+		if(t.myLeft!=null){
+			writeTree(t.myLeft, out);
+		}
+		if(t.myRight!=null){
+			writeTree(t.myRight, out);
+		}
+	}
+	public void writeCompressedBits(BitInputStream in, String[] codings, BitOutputStream out){
+		int bit = 0;
+		while((bit= in.readBits(BITS_PER_WORD))!= -1){
+			
+			String encode = codings[bit];
+			out.writeBits(encode.length(), Integer.parseInt(encode, 2));
+			
+		}
+		out.writeBits(codings[PSEUDO_EOF].length(), Integer.parseInt(codings[PSEUDO_EOF], 2));
+	}
+	
 	/**
 	 * Decompresses a file. Output file must be identical bit-by-bit to the
 	 * original.
@@ -47,14 +130,48 @@ public class HuffProcessor {
 	 * @param out
 	 *            Buffered bit stream writing to the output file.
 	 */
+	
 	public void decompress(BitInputStream in, BitOutputStream out){
-	    while (true){
-            int val = in.readBits(BITS_PER_WORD);
-            if (val == -1) break;
-            
-            out.writeBits(BITS_PER_WORD, val);
-        }
+	    int id = in.readBits(BITS_PER_INT);
+	    // check id to see if valid compressed file
+	    if(id!=HUFF_TREE) throw new HuffException("not correct file");
+
+
+	    TreeNode root = readTreeHeader(in);
+	    readCompressedBits(root,in,out);
 	}
+	public TreeNode readTreeHeader(BitInputStream in){
+		
+		if(in.readBits(1)==0){
+			TreeNode left = readTreeHeader(in);
+			TreeNode right = readTreeHeader(in);
+			return new TreeNode(0,0, left, right);
+		}
+		else{
+			return new TreeNode(in.readBits(9),-1);
+			}
+	}
+	public void readCompressedBits(TreeNode root, BitInputStream in, BitOutputStream out){
+		TreeNode current = root;
+		int bit = 0;
+		while((bit=in.readBits(1))!=-1){
+			
+			if(current.myLeft==null && current.myRight==null){
+				if(current.myValue==PSEUDO_EOF) break;
+				out.writeBits(8, current.myValue);
+				current = root;
+				
+				
+				}
+			if(bit==0){
+				current=current.myLeft;
+			}
+			else{
+				current= current.myRight;
+			}
+			}
+		}
+	
 	
 	public void setHeader(Header header) {
         myHeader = header;
